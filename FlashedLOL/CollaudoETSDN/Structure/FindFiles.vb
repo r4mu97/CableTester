e7a307e12fd6591fdfec48bd4d39ec28918f66a8
Riccardo Mucci <riccardo.mucci@kiwitron.it>
2023-02-01 15:00:59 +0100

    add a function to fill the list of cables code

--- FlashedLOL/CollaudoETSDN/Structure/FindFiles.vb
+++ FlashedLOL/CollaudoETSDN/Structure/FindFiles.vb
@@ -1,3 +1,24 @@
-﻿Public Class FindFiles
+﻿
+Imports Microsoft.VisualBasic.FileIO
+Imports Microsoft.VisualBasic.Logging
+Imports System.IO
+Public Class FindFiles
+
+    Public Sub New()
+
+    End Sub
+
+    Public Function Find_Files(gui As Main)
+        Dim files() As String = IO.Directory.GetFiles(Application.StartupPath + "//CablesList")
+
+        gui.cbox_list_cables.Items.Clear()
+        For Each file As String In files
+            gui.cbox_list_cables.Items.Add(file.Substring(61))
+        Next
+
+    End Function
+
+
+
 
 End Class
