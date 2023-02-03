fd2749b6938172b4b89ac161d6ceb5809ca20b7e
Riccardo Mucci <riccardo.mucci@kiwitron.it>
2023-02-03 15:50:49 +0100

    added all of output to send to the PLC

--- FlashedLOL/CollaudoETSDN/CanComunicaation/SendVar_to_PLC.vb
+++ FlashedLOL/CollaudoETSDN/CanComunicaation/SendVar_to_PLC.vb
@@ -1,62 +1,76 @@
 ﻿Public Class SendVar_to_PLC
-    Dim setRL1, setRL2, setRL3, setAllRl, setSupplyRl1, setSupplyRl2, setSupplyRl3, setIP1, setIP2, setIN1, setIN2, set10V, Set5V As Byte
+    Dim setOut1, setOut2, setOut3, setOut4, setOut5, setOut6, setOut7, setOut8, setOut9, setOut10,
+        setOut11, setOut12, setOut13, setOut14, setOut15, setOut16, setOut17, setOut18, setOut19, setOut20 As Byte
 
-
-    Public Sub SetStatePin1(stato)
-        setRL1 = stato
-    End Sub
-
-    Public Sub SetStatePin2(stato)
-        setRL2 = stato
+    Public Sub SetStateOut1(stato)
+        setOut1 = stato
     End Sub
 
-    Public Sub SetStatePin3(stato)
-        setRL3 = stato
+    Public Sub SetStateOut2(stato)
+        setOut2 = stato
     End Sub
 
-    Public Sub SetStatePin4(stato)
-        setRL1 = stato
-        setRL2 = stato
-        setRL3 = stato
+    Public Sub SetStateOut3(stato)
+        setOut3 = stato
     End Sub
 
-    Public Sub SetPwrRL1(stato)
-        setSupplyRl1 = stato
+    Public Sub SetStateOut4(stato)
+        setOut4 = stato
     End Sub
 
-    Public Sub SetPwrRL2(stato)
-        setSupplyRl2 = stato
+    Public Sub SetStateOut5(stato)
+        setOut5 = stato
     End Sub
 
-    Public Sub SetPwrRL3(stato)
-        setSupplyRl3 = stato
-    End Sub
-    Public Sub SetAllPwrRL(stato)
-        setSupplyRl1 = stato
-        setSupplyRl2 = stato
-        setSupplyRl3 = stato
-    End Sub
-    Public Sub SetState10V(stato)
-        set10V = stato
+    Public Sub SetStateOut6(stato)
+        setOut6 = stato
     End Sub
 
-    Public Sub SetState5V(stato)
-        Set5V = stato
+    Public Sub SetStateOut7(stato)
+        setOut7 = stato
     End Sub
-    Public Sub SetStateIP1(stato)
-        setIP1 = stato
+    Public Sub SetStateOut8(stato)
+        setOut8 = stato
     End Sub
-
-    Public Sub SetStateIP2(stato)
-        setIP2 = stato
-    End Sub
-
-    Public Sub SetStateIN1(stato)
-        setIN1 = stato
+    Public Sub SetStateOut9(stato)
+        setOut9 = stato
     End Sub
 
-    Public Sub SetStateIN2(stato)
-        setIN2 = stato
+    Public Sub SetStateOut10(stato)
+        setOut10 = stato
+    End Sub
+    Public Sub SetStateOut11(stato)
+        setOut11 = stato
+    End Sub
+
+    Public Sub SetStateOut12(stato)
+        setOut12 = stato
+    End Sub
+
+    Public Sub SetStateOut13(stato)
+        setOut13 = stato
+    End Sub
+
+    Public Sub SetStateOut14(stato)
+        setOut14 = stato
+    End Sub
+    Public Sub SetStateOut15(stato)
+        setOut15 = stato
+    End Sub
+    Public Sub SetStateOut16(stato)
+        setOut16 = stato
+    End Sub
+    Public Sub SetStateOut17(stato)
+        setOut17 = stato
+    End Sub
+    Public Sub SetStateOut18(stato)
+        setOut18 = stato
+    End Sub
+    Public Sub SetStateOut19(stato)
+        setOut19 = stato
+    End Sub
+    Public Sub SetStateOut20(stato)
+        setOut20 = stato
     End Sub
 
 
@@ -64,20 +78,20 @@
         Dim VarTempComposer As UShort
 
         VarTempComposer = 0
-        VarTempComposer = VarTempComposer + setRL1
-        VarTempComposer = VarTempComposer + (CType(setRL2, UShort) << 1)
-        VarTempComposer = VarTempComposer + (CType(setRL3, UShort) << 2)
-        VarTempComposer = VarTempComposer + (CType(setSupplyRl1, UShort) << 3)
-        VarTempComposer = VarTempComposer + (CType(setSupplyRl2, UShort) << 4)
-        VarTempComposer = VarTempComposer + (CType(setSupplyRl3, UShort) << 5)
-        VarTempComposer = VarTempComposer + (CType(set10V, UShort) << 6)
-        VarTempComposer = VarTempComposer + (CType(Set5V, UShort) << 7)
+        VarTempComposer = VarTempComposer + setOut1
+        VarTempComposer = VarTempComposer + (CType(setOut1, UShort) << 1)
+        VarTempComposer = VarTempComposer + (CType(setOut2, UShort) << 2)
+        VarTempComposer = VarTempComposer + (CType(setOut3, UShort) << 3)
+        VarTempComposer = VarTempComposer + (CType(setOut4, UShort) << 4)
+        VarTempComposer = VarTempComposer + (CType(setOut5, UShort) << 5)
+        VarTempComposer = VarTempComposer + (CType(setOut6, UShort) << 6)
+        VarTempComposer = VarTempComposer + (CType(setOut7, UShort) << 7)
 
 
-        VarTempComposer = VarTempComposer + (CType(setIP1, UShort) << 8)
-        VarTempComposer = VarTempComposer + (CType(setIP2, UShort) << 9)
-        VarTempComposer = VarTempComposer + (CType(setIN1, UShort) << 10)
-        VarTempComposer = VarTempComposer + (CType(setIN2, UShort) << 11)
+        VarTempComposer = VarTempComposer + (CType(setOut8, UShort) << 8)
+        VarTempComposer = VarTempComposer + (CType(setOut9, UShort) << 9)
+        VarTempComposer = VarTempComposer + (CType(setOut10, UShort) << 10)
+        VarTempComposer = VarTempComposer + (CType(setOut11, UShort) << 11)
 
         Return VarTempComposer
     End Function
