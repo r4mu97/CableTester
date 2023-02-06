08cecda1cbb08c7f24b2c781d59760d76d22931e
Riccardo Mucci <riccardo.mucci@kiwitron.it>
2023-02-06 18:41:49 +0100

    Added a function to read line to line and split it

--- FlashedLOL/CollaudoETSDN/Functions/ReadFile.vb
+++ FlashedLOL/CollaudoETSDN/Functions/ReadFile.vb
@@ -2,6 +2,7 @@
 Imports Microsoft.VisualBasic.FileIO
 Imports MongoDB.Driver
 Imports PdfSharp.Charting
+Imports SharpCompress.Common
 Imports System.IO
 Public Class ReadFile
 
@@ -13,15 +14,50 @@ Public Class ReadFile
 
     Public Function Read_configCable_file(code_cable_file As String)
         code_cable_file = code_cable_file + ".txt"
-        Dim myFileLines As List(Of String) = File.ReadAllLines(directory_of_codeCable + code_cable_file).ToList
-        Dim lines As String() = IO.File.ReadAllLines(directory_of_codeCable + code_cable_file)
-        Dim list_of_data As New List(Of String())
+        'Dim myFileLines As List(Of String) = File.ReadAllLines(directory_of_codeCable + code_cable_file).ToList
+        'Dim lines As String() = IO.File.ReadAllLines(directory_of_codeCable + code_cable_file)
+        ''Dim list_of_data As New List(Of String())
 
-        If Not lines Is Nothing Then
-            For Each line In lines
-                list_of_data.Add(line.Split(" "c))
+        'If Not lines Is Nothing Then
+        '    For Each line In lines
+        '        list_of_data.AddRange(line.Split("-"c))
+        '    Next
+        'End If
+        'Dim lines As List(Of String) = New List(Of String)
+
+        'If File.Exists((directory_of_codeCable + code_cable_file)) Then
+        '    Using reader As StreamReader = File.OpenText((directory_of_codeCable + code_cable_file))
+        '        Dim line As String
+        '        While Not reader.EndOfStream
+        '            line = reader.ReadLine()
+        '            lines.AddRange(line.Split(","c))
+        '        End While
+        '    End Using
+        'End If
+
+        'Return lines
+
+
+        Dim lines As List(Of String) = New List(Of String)
+
+        If File.Exists(directory_of_codeCable + code_cable_file) Then
+            Dim fileLines = File.ReadAllLines(directory_of_codeCable + code_cable_file)
+
+            For Each line In fileLines
+                lines.AddRange(line.Split(","c))
+                Assigne_the_lines(lines, Main)
             Next
         End If
+
+        Return lines
+    End Function
+
+    Public Function Assigne_the_lines(list_of_data As List(Of String), gui As Main)
+        Dim i As Int16
+        For Each datas In list_of_data
+
+            gui.ChangeControlRichText(gui.info_text_box, list_of_data(0), "white")
+        Next
 
         Return list_of_data
     End Function
